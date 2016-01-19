8a3dba1e72a8562866de7b8c88faf91900cc7714
Jake Runzer <jakerunzer@gmail.com>
2016-01-18 19:07:41 -0800

    init commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var liveServer = require("live-server");
+
+var params = {
+  port: 8181,
+  host: "0.0.0.0", // Set the address to bind to. Defaults to 0.0.0.0.
+  root: "./", // Set root directory that's being server. Defaults to cwd.
+  open: false, // When false, it won't load your browser by default.
+  ignore: 'assets/sass', // comma-separated string for paths to ignore
+  file: "404.html", // When set, serve this file for every 404 (useful for single-page applications)
+  wait: 0 // Waits for all changes, before reloading. Defaults to 0 sec.
+};
+liveServer.start(params);
